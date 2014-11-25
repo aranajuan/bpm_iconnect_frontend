1cdab81201b63526236de20c9d9f96249f0db8c9
Arana Juan <arana.juanpablo@gmail.com>
2014-11-25 11:45:17 -0300

    Cambio efecto menu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,18 @@
-function menu_go(dest){
-    
-    location.href="?L="+dest+"&m=menu";
-    
+function menu_go(dest) {
+
+    location.href = "?L=" + dest + "&m=menu";
 }
 
-function menu_sub(id){
-    $(".mainmenu").hide();
-    $(".submenu").hide();
-    $("#"+id).show();
+function menu_sub(id) {
+    $(".mainmenu").fadeOut(100, function () {
+        $(".submenu").hide();
+        $("#" + id).fadeIn();
+    });
 }
 
-function menu_main(){
-    $(".submenu").hide();
-    $(".mainmenu").show();
+function menu_main() {
+    $(".submenu").fadeOut(100,function(){
+         $(".mainmenu").fadeIn();
+    });
+   
 }
